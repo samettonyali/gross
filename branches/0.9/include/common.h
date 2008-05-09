c504b7f5835625fc28d5de9292d823b0c9217ef7
eino.tuominen <eino.tuominen@529a611b-dd24-0410-8390-ad8781149581>
2008-05-09 20:50:23 +0000

    Merge 408 from trunk: statefile test

--- branches/0.9/include/common.h
+++ branches/0.9/include/common.h
@@ -251,6 +251,11 @@ typedef struct
 	void *check_arg;
 } check_t;
 
+typedef struct statefile_info_s
+{
+	int fd;
+} statefile_info_t;
+
 typedef struct
 {
 	bloom_ring_queue_t *filter;
@@ -266,6 +271,7 @@ typedef struct
 #endif				/* ENDBL */
 	gross_config_t config;
 	mmapped_brq_t *mmap_info;
+	statefile_info_t *statefile_info;
 	thread_collection_t process_parts;
 	stats_t stats;
 	check_t *checklist[MAXCHECKS];
